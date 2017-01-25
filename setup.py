6778e31ceffd6ebca495962672a39bdfbc6e2ad3
Raphaël Stefanini <rphl@rphl.net>
2017-01-25 17:52:00 +0200

    update download path

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name = 'godnames',
   packages = ['godnames'],
-  version = '1.0',
+  version = '1.1',
   description = 'A library to generate randomly names',
   author = 'Raphael Stefanini',
   author_email = 'rphl@rphl.net',
   url = 'https://github.com/rphlo/py-godnames',
-  download_url = 'https://github.com/rphlo/py-godnames/tarball/1.0',
+  download_url = 'https://github.com/rphlo/py-godnames/tarball/1.1',
   keywords = ['random', 'naming', 'name'],
   classifiers = [],
 )
