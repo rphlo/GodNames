7f2ebc9be936cda29789650f2ad5f5290a498bc0
Raphaël Stefanini <raphael.stefanini@payiq.net>
2017-01-26 08:32:16 +0200

    Add command line tool 'godname'

--- setup.py
+++ setup.py
@@ -3,12 +3,20 @@ from setuptools import setup
 setup(
   name = 'godnames',
   packages = ['godnames'],
-  version = '1.1',
+  version = '1.1.1',
   description = 'A library to generate randomly names',
   author = 'Raphael Stefanini',
   author_email = 'rphl@rphl.net',
   url = 'https://github.com/rphlo/py-godnames',
-  download_url = 'https://github.com/rphlo/py-godnames/tarball/1.1',
+  download_url = 'https://github.com/rphlo/py-godnames/tarball/1.1.1',
   keywords = ['random', 'naming', 'name'],
-  classifiers = [],
+  classifiers = [
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+  ],
+  entry_points = {
+    'console_scripts': ['godname=godnames.command_line:main'],
+  }
 )
